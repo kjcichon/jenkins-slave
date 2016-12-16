947f188c8b3ae1734cd2237c3d2312ddcadb5dae
Kevin Cichon <kcichon@follett.com>
2016-12-16 08:19:53 -0600

    Home dir is var/go not home/go

--- node6-agent/start_agent_containers.sh
+++ node6-agent/start_agent_containers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-   sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v /etc/ansible:/etc/ansible -v /home/go/.ssh:/var/go/.ssh -d -e GO_SERVER=gocd.fsc.follett.com -e GO_SERVER_URL=https://gocd.fsc.follett.com -e AGENT_KEY=123456789abcdef -e AGENT_RESOURCES=docker_created,kube-deployer,node_6 fss/gocd-agent-node6:latest
+   sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v /etc/ansible:/etc/ansible -v /var/go/.ssh:/var/go/.ssh -d -e GO_SERVER=gocd.fsc.follett.com -e GO_SERVER_URL=https://gocd.fsc.follett.com -e AGENT_KEY=123456789abcdef -e AGENT_RESOURCES=docker_created,kube-deployer,node_6 fss/gocd-agent-node6:latest
