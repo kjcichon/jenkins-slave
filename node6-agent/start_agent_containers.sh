86c3ff390c71a08732d8b3174fd6d8ce3a7fc1f2
Kevin Cichon <kcichon@follett.com>
2016-12-21 16:07:49 -0600

    fixed start command

--- node6-agent/start_agent_containers.sh
+++ node6-agent/start_agent_containers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-   sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v /etc/ansible:/etc/ansible -v /opt/goAgentSshKeys:/var/go/.ssh -d -e GO_SERVER=gocd.fsc.follett.com -e GO_SERVER_URL=https://gocd.fsc.follett.com -e AGENT_KEY=123456789abcdef -e AGENT_RESOURCES=docker_created,kube-deployer,node_6 docker push dockerhub.follettsoftware.com/fss/gocd-agent-node6:latest
+   sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v /etc/ansible:/etc/ansible -v /opt/goAgentSshKeys:/var/go/.ssh -d -e GO_SERVER=gocd.fsc.follett.com -e GO_SERVER_URL=https://gocd.fsc.follett.com -e AGENT_KEY=123456789abcdef -e AGENT_RESOURCES=docker_created,kube-deployer,node_6 dockerhub.follettsoftware.com/fss/gocd-agent-node6:latest
