516663d099ab9896c1e1e9af966c9fd6166dd899
Kevin Cichon <kcichon@follett.com>
2016-12-21 11:13:52 -0600

    Fix to push and pull container.

--- node6-agent/build_docker_image.sh
+++ node6-agent/build_docker_image.sh
@@ -2,4 +2,5 @@
 
 
 sudo docker login -u devadmin -p Stuffy2 dockerhub.follettsoftware.com
-sudo docker build -t fss/gocd-agent-node6:latest .
+sudo docker build -t dockerhub.follettsoftware.com/fss/gocd-agent-node6:latest .
+sudo docker push dockerhub.follettsoftware.com/fss/gocd-agent-node6:latest
