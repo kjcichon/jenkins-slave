3d78dc073084a1f34a33f9a268904c6920e7cb9e
Kevin Cichon <kcichon@follett.com>
2016-12-22 13:47:25 -0600

    don't push,; pull local image.

--- maven-agent/build_docker_image.sh
+++ maven-agent/build_docker_image.sh
@@ -3,4 +3,4 @@
 
 sudo docker login -u devadmin -p Stuffy2 dockerhub.follettsoftware.com
 sudo docker build -t dockerhub.follettsoftware.com/fss/gocd-agent-maven3:latest .
-sudo docker push dockerhub.follettsoftware.com/fss/gocd-agent-maven3:latest
+#sudo docker push dockerhub.follettsoftware.com/fss/gocd-agent-maven3:latest
--- node6-agent/build_docker_image.sh
+++ node6-agent/build_docker_image.sh
@@ -3,4 +3,4 @@
 
 sudo docker login -u devadmin -p Stuffy2 dockerhub.follettsoftware.com
 sudo docker build -t dockerhub.follettsoftware.com/fss/gocd-agent-node6:latest .
-sudo docker push dockerhub.follettsoftware.com/fss/gocd-agent-node6:latest
+#sudo docker push dockerhub.follettsoftware.com/fss/gocd-agent-node6:latest
