0763c93978b3251ac3330518b185902a95cfa3bd
Kevin Cichon <kcichon@follett.com>
2016-12-22 13:56:27 -0600

    fixed image name.

--- maven-agent/start_agent_containers.sh
+++ maven-agent/start_agent_containers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-   sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v /etc/ansible:/etc/ansible -v /opt/goAgentSshKeys:/var/go/.ssh -d -e GO_SERVER=gocd.fsc.follett.com -e GO_SERVER_URL=https://gocd.fsc.follett.com -e AGENT_KEY=123456789abcdef -e AGENT_RESOURCES=docker_created,maven3,java8,ansible,kube-deployer fss/gocd-agent-maven3:latest
+   sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v /etc/ansible:/etc/ansible -v /opt/goAgentSshKeys:/var/go/.ssh -d -e GO_SERVER=gocd.fsc.follett.com -e GO_SERVER_URL=https://gocd.fsc.follett.com -e AGENT_KEY=123456789abcdef -e AGENT_RESOURCES=docker_created,maven3,java8,ansible,kube-deployer dockerhub.follettsoftware.com/fss/gocd-agent-maven3:latest
